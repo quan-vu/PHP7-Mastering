c3636ac14d7f460272f4132203ab4894cd66305f
Quan Vu <huyquan.vu.1@gmail.com>
2021-07-17 21:47:29 +0700

    [PHP New features] - Group use declarations

---
+++ 01-php7-keyfeatures/use_group_imports.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * PHP 7 
+ */
+use Magento\Backend\Block\Widget\Grid;
+use Magento\Backend\Block\Widget\Grid\{
+  Column,
+  Extended
+};
+
+use Magento\Backend\Block\Widget\{
+  Grid
+  Grid\Column,
+  Grid\Extended
+};
+
+ /**
+  * Old way
+  */
+
+use Magento\Backend\Block\Widget\Grid;
+use Magento\Backend\Block\Widget\Grid\Column;
+use Magento\Backend\Block\Widget\Grid\Extended;
