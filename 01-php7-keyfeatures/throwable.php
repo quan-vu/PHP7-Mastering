343049963cace530838d16e209c75b7eb477029b
Quan Vu <huyquan.vu.1@gmail.com>
2021-07-17 21:47:19 +0700

    [PHP New features] - Throwable Exception

---
+++ 01-php7-keyfeatures/throwable.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Throwables an Exception
+ */
+
+ // Case 1
+try {
+    $mailer = new Mailer($transport);
+} catch (\Throwable $e) {
+    echo 'Caught!';
+} finally {
+    echo 'Cleanup!';
+}
+
+// Case 2
+try {
+    $mailer = new Mailer($transport);
+} catch (\Error $e) {
+    echo 'Caught!';
+} finally {
+    echo 'Cleanup!';
+}
